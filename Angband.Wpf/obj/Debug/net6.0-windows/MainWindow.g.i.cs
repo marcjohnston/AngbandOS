a62b7dd50ca8d6f68d418a2a3c6cf935d09bdb3f
Marc Johnston <marc-johnston@outlook.com>
2022-08-14 19:56:23 -0400

    Renamed the Angband.Wpf to AngbandOS.Wpf

--- Angband.Wpf/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Angband.Wpf/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace Angband.Wpf {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Angband.Wpf;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AngbandOS.Wpf;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
