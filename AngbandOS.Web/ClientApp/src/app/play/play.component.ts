ef46dde2d81fcbdf3ba50293e2af0f12594fd409
Marc Johnston <marc-johnston@outlook.com>
2022-08-25 00:21:48 -0400

    Sounds are working now.

--- AngbandOS.Web/ClientApp/src/app/play/play.component.ts
+++ AngbandOS.Web/ClientApp/src/app/play/play.component.ts
@@ -8,6 +8,135 @@ import { AuthenticationService } from '../accounts/authentication-service/authen
 import { UserDetails } from '../accounts/authentication-service/user-details';
 
 const charSize = 18;
+enum SoundEffect {
+  EnterTownDay,
+  EnterTownNight,
+  EnterDungeon1,
+  EnterDungeon2,
+  EnterDungeon3,
+  EnterDungeon4,
+  EnterDungeon5,
+  StoreSoldWorthless,
+  StoreSoldBargain,
+  StoreSoldCheaply,
+  StoreSoldExtraCheaply,
+  StoreTransaction,
+  EnterStore,
+  LeaveStore,
+  MeleeHit,
+  HitGood,
+  HitGreat,
+  HitSuperb,
+  HitHiGreat,
+  HitHiSuperb,
+  Miss,
+  Shoot,
+  RangedHit,
+  HealthWarning,
+  PlayerDeath,
+  PlayerBlind,
+  Playerconfused,
+  PlayerPoisoned,
+  PlayerAfraid,
+  PlayerParalysed,
+  PlayerDrugged,
+  PlayerSlow,
+  PlayerCut,
+  PlayerStatDrain,
+  PlayerRecover,
+  PlayerHaste,
+  PlayerShield,
+  PlayerBlessed,
+  PlayerHeroism,
+  PlayerBerserk,
+  PlayerProtEvil,
+  PlayerInvulnerable,
+  PlayerSeeInvisible,
+  PlayerInfravision,
+  PlayerResistAcid,
+  PlayerResistElectric,
+  PlayerResistFire,
+  PlayerResistCold,
+  PlayerResistPoison,
+  Hungry,
+  PickUpMoney1,
+  PickUpMoney2,
+  PickUpMoney3,
+  Drop,
+  LevelGain,
+  Study,
+  Teleport,
+  Quaff,
+  ZapRod,
+  BumpWall,
+  Eat,
+  Dig,
+  OpenDoor,
+  ShutDoor,
+  TeleportLevel,
+  Bell,
+  NothingToOpen,
+  LockpickFail,
+  LockpickSuccess,
+  DisarmTrap,
+  StairsUp,
+  StairsDown,
+  ActivateArtifact,
+  UseStaff,
+  DestroyItem,
+  WieldWeapon,
+  Cursed,
+  FindSecretDoor,
+  PseudoId,
+  CastSpell,
+  Pray,
+  MonsterFlees,
+  MonsterDies,
+  UniqueDies,
+  QuestMonsterDies,
+  MonsterHits,
+  MonsterTouches,
+  MonsterBashes,
+  MonsterKicks,
+  MonsterClaws,
+  MonsterBites,
+  MonsterStings,
+  MonsterButts,
+  MonsterCrushes,
+  MonsterEngulfs,
+  MonsterCrawls,
+  MonsterDrools,
+  MonsterSpits,
+  MonsterGazes,
+  MonsterWails,
+  MonsterSpores,
+  MonsterBegs,
+  MonsterInsults,
+  MonsterMoans,
+  MonsterShrieks,
+  MonsterCreatesTraps,
+  MonsterCausesFear,
+  MonsterBreeds,
+  SummonMonster,
+  SummonAngel,
+  SummonUndead,
+  SummonAnimals,
+  SummonSpiders,
+  SummonHounds,
+  SummonHydras,
+  SummonDemons,
+  SummonDragons,
+  SummonGreaterUndead,
+  SummonGreaterDragons,
+  SummonGreaterDemons,
+  SummonRingwraiths,
+  SummonUniques,
+  BreathWeapon,
+  IdentifyBad,
+  IdentifyGood,
+  IdentifySpecial
+}
+
 
 @Component({
   selector: 'app-play',
@@ -22,6 +151,7 @@ export class PlayComponent implements OnInit, OnDestroy {
   public connectionId: string | null = null;
   public gameGuid: string | null | undefined = undefined; // Represents the unique identifier for the game to play; null, to start a new game; otherwise, undefined when the guid hasn't been retrieved yet.
   private _initSubscriptions = new Subscription();
+  private _sounds = new Map<number, string[]>();
 
   constructor(
     private _httpClient: HttpClient,
@@ -87,7 +217,136 @@ export class PlayComponent implements OnInit, OnDestroy {
 
   public get charSize(): number {
     return charSize;
-}
+  }
+
+  private setupSounds() {
+    this._sounds.set(SoundEffect.ActivateArtifact, [ "plm_aim_wand.wav"]);
+    this._sounds.set(SoundEffect.Bell, [ "plm_jar_ding.wav"]);
+    this._sounds.set(SoundEffect.BreathWeapon, [ "mco_attack_breath.wav"]);
+    this._sounds.set(SoundEffect.BumpWall, [ "plm_wood_thud.wav"]);
+    this._sounds.set(SoundEffect.CastSpell, [ "plm_spell1.wav", "plm_spell2.wav", "plm_spell3.wav"]);
+    this._sounds.set(SoundEffect.Cursed, [ "pls_man_oooh.wav"]);
+    this._sounds.set(SoundEffect.DestroyItem, [ "plm_bang_metal.wav", "plm_break_canister.wav", "plm_break_glass.wav", "plm_break_glass2.wav", "plm_break_plates.wav", "plm_break_shatter.wav", "plm_break_smash.wav", "plm_glass_breaking.wav", "plm_glass_break.wav", "plm_glass_smashing.wav"]);
+    this._sounds.set(SoundEffect.Dig, [ "plm_metal_clank.wav"]);
+    this._sounds.set(SoundEffect.DisarmTrap, [ "plm_bang_ceramic.wav", "plm_chest_latch.wav", "plm_click_switch3.wav"]);
+    this._sounds.set(SoundEffect.Drop, [ "plm_drop_boot.wav"]);
+    this._sounds.set(SoundEffect.Eat, [ "plm_eat_bite.wav"]);
+    this._sounds.set(SoundEffect.EnterDungeon1, [ "amb_door_iron.wav", "amb_bell_metal1.wav"]);
+    this._sounds.set(SoundEffect.EnterDungeon2, [ "amb_bell_tibet1.wav", "amb_bell_metal2.wav", "amb_gong_strike.wav"]);
+    this._sounds.set(SoundEffect.EnterDungeon3, [ "amb_bell_tibet2.wav", "amb_dungeon_echo.wav", "amb_pulse_low.wav"]);
+    this._sounds.set(SoundEffect.EnterDungeon4, [ "amb_bell_tibet3.wav", "amb_dungeon_echowet.wav", "amb_gong_undertone.wav"]);
+    this._sounds.set(SoundEffect.EnterDungeon5, [ "amb_door_doom.wav", "amb_gong_chinese.wav", "amb_gong_low.wav"]);
+    this._sounds.set(SoundEffect.EnterStore, [ "sto_bell_desk.wav", "sto_bell_ding.wav", "sto_bell_dingaling.wav", "sto_bell_jingles.wav", "sto_bell_ringing.wav", "sto_bell_shop.wav"]);
+    this._sounds.set(SoundEffect.EnterTownDay, [ "amb_thunder_rain.wav"]);
+    this._sounds.set(SoundEffect.EnterTownNight, [ "amb_guitar_chord.wav", "amb_thunder_roll.wav"]);
+    this._sounds.set(SoundEffect.FindSecretDoor, [ "id_bad_hmm.wav"]);
+    this._sounds.set(SoundEffect.HealthWarning, [ "plc_bell_warn.wav"]);
+    this._sounds.set(SoundEffect.HitGood, [ "plc_hit_anvil.wav"]);
+    this._sounds.set(SoundEffect.HitGreat, [ "plc_hit_groan.wav"]);
+    this._sounds.set(SoundEffect.HitHiGreat, [ "plc_hit_grunt.wav"]);
+    this._sounds.set(SoundEffect.HitHiSuperb, [ "plc_hit_anvil2.wav"]);
+    this._sounds.set(SoundEffect.HitSuperb, [ "plc_hit_grunt2.wav"]);
+    this._sounds.set(SoundEffect.Hungry, [ "id_bad_hmm.wav"]);
+    this._sounds.set(SoundEffect.IdentifyBad, [ "id_bad_hmm.wav"]);
+    this._sounds.set(SoundEffect.IdentifyGood, [ "id_bad_hmm.wav"]);
+    this._sounds.set(SoundEffect.IdentifySpecial, [ "id_bad_hmm.wav"]);
+    this._sounds.set(SoundEffect.LeaveStore, [ "plm_door_bolt.wav"]);
+    this._sounds.set(SoundEffect.LevelGain, [ "plm_levelup.wav"]);
+    this._sounds.set(SoundEffect.LockpickFail, [ "plm_click_dry.wav", "plm_click_switch.wav", "plm_click_wood.wav", "plm_door_echolock.wav", "plm_door_wooden.wav"]);
+    this._sounds.set(SoundEffect.LockpickSuccess, [ "plm_break_wood.wav", "plm_cabinet_open.wav", "plm_chest_unlatch.wav", "plm_lock_case.wav", "plm_lock_distant.wav", "plm_open_case.wav"]);
+    this._sounds.set(SoundEffect.MeleeHit, [ "plc_hit_hay.wav", "plc_hit_body.wav"]);
+    this._sounds.set(SoundEffect.Miss, [ "plc_miss_arrow2.wav"]);
+    this._sounds.set(SoundEffect.MonsterBashes, [ "mco_squish_snap.wav"]);
+    this._sounds.set(SoundEffect.MonsterBegs, [ "mco_man_mumble.wav"]);
+    this._sounds.set(SoundEffect.MonsterBites, [ "mco_snarl_short.wav", "mco_bite_soft.wav", "mco_bite_munch.wav", "mco_bite_long.wav", "mco_bite_short.wav", "mco_bite_gnash.wav", "mco_bite_chomp.wav", "mco_bite_regular.wav", "mco_bite_small.wav", "mco_bite_dainty.wav", "mco_bite_hard.wav", "mco_bite_chew.wav"]);
+    this._sounds.set(SoundEffect.MonsterBreeds, [ "mco_frog_trill.wav"]);
+    this._sounds.set(SoundEffect.MonsterButts, [ "mco_cuica_rubbing.wav", "mco_thud_crash.wav"]);
+    this._sounds.set(SoundEffect.MonsterCausesFear, [ "mco_creature_groan.wav", "mco_dino_slur.wav"]);
+    this._sounds.set(SoundEffect.MonsterClaws, [ "mco_ceramic_trill.wav", "mco_scurry_dry.wav"]);
+    this._sounds.set(SoundEffect.MonsterCrawls, [ "mco_card_shuffle.wav", "mco_shake_roll.wav"]);
+    this._sounds.set(SoundEffect.MonsterCreatesTraps, [ "mco_thoing_deep.wav"]);
+    this._sounds.set(SoundEffect.MonsterCrushes, [ "mco_dino_low.wav", "mco_squish_hit.wav"]);
+    this._sounds.set(SoundEffect.MonsterDies, [ "mco_howl_croak.wav", "mco_howl_deep.wav", "mco_howl_distressed.wav", "mco_howl_high.wav", "mco_howl_long.wav"]);
+    this._sounds.set(SoundEffect.MonsterDrools, [ "mco_creature_choking.wav", "mco_liquid_squirt.wav"]);
+    this._sounds.set(SoundEffect.MonsterEngulfs, [ "mco_dino_talk.wav", "mco_dino_yawn.wav"]);
+    this._sounds.set(SoundEffect.MonsterFlees, [ "mco_creature_yelp.wav"]);
+    this._sounds.set(SoundEffect.MonsterGazes, [ "mco_thoing_backwards.wav"]);
+    this._sounds.set(SoundEffect.MonsterHits, [ "mco_hit_whip.wav"]);
+    this._sounds.set(SoundEffect.MonsterInsults, [ "mco_strange_thwoink.wav"]);
+    this._sounds.set(SoundEffect.MonsterKicks, [ "mco_rubber_thud.wav"]);
+    this._sounds.set(SoundEffect.MonsterMoans, [ "mco_strange_music.wav"]);
+    this._sounds.set(SoundEffect.MonsterShrieks, [ "mco_mouse_squeaks.wav"]);
+    this._sounds.set(SoundEffect.MonsterSpits, [ "mco_attack_spray.wav"]);
+    this._sounds.set(SoundEffect.MonsterSpores, [ "mco_dub_wobble.wav", "mco_spray_long.wav"]);
+    this._sounds.set(SoundEffect.MonsterStings, [ "mco_castanet_trill.wav", "mco_tube_hit.wav"]);
+    this._sounds.set(SoundEffect.MonsterTouches, [ "mco_click_vibra.wav"]);
+    this._sounds.set(SoundEffect.MonsterWails, [ "mco_dino_low.wav"]);
+    this._sounds.set(SoundEffect.NothingToOpen, [ "plm_click_switch2.wav", "plm_door_knob.wav"]);
+    this._sounds.set(SoundEffect.OpenDoor, [ "plm_door_bolt.wav", "plm_door_creak.wav", "plm_door_dungeon.wav", "plm_door_entrance.wav", "plm_door_open.wav", "plm_door_opening.wav", "plm_door_rusty.wav", "plm_door_squeaky.wav"]);
+    this._sounds.set(SoundEffect.PickUpMoney1, [ "plm_coins_light.wav", "plm_coins_shake.wav"]);
+    this._sounds.set(SoundEffect.PickUpMoney2, [ "plm_chain_light.wav", "plm_coins_pour.wav"]);
+    this._sounds.set(SoundEffect.PickUpMoney3, [ "plm_coins_dump.wav"]);
+    this._sounds.set(SoundEffect.PlayerAfraid, [ "pls_man_yell.wav"]);
+    this._sounds.set(SoundEffect.PlayerBerserk, [ "pls_man_scream2.wav"]);
+    this._sounds.set(SoundEffect.PlayerBlessed, [ "sum_angel_song.wav"]);
+    this._sounds.set(SoundEffect.PlayerBlind, [ "pls_tone_conk.wav"]);
+    this._sounds.set(SoundEffect.Playerconfused, [ "pls_man_ugh.wav"]);
+    this._sounds.set(SoundEffect.PlayerCut, [ "pls_man_argoh.wav"]);
+    this._sounds.set(SoundEffect.PlayerDeath, [ "plc_die_laugh.wav"]);
+    this._sounds.set(SoundEffect.PlayerDrugged, [ "pls_breathe_in.wav"]);
+    this._sounds.set(SoundEffect.PlayerHaste, [ "pls_bell_sustain.wav"]);
+    this._sounds.set(SoundEffect.PlayerHeroism, [ "pls_tone_goblet.wav"]);
+    this._sounds.set(SoundEffect.PlayerInfravision, [ "pls_tone_clavelo8.wav"]);
+    this._sounds.set(SoundEffect.PlayerInvulnerable, [ "pls_tone_blurk.wav"]);
+    this._sounds.set(SoundEffect.PlayerParalysed, [ "pls_man_gulp_new.wav"]);
+    this._sounds.set(SoundEffect.PlayerPoisoned, [ "pls_tone_guiro.wav"]);
+    this._sounds.set(SoundEffect.PlayerProtEvil, [ "pls_bell_glass.wav"]);
+    this._sounds.set(SoundEffect.PlayerRecover, [ "pls_bell_chime_new.wav"]);
+    this._sounds.set(SoundEffect.PlayerResistAcid, [ "pls_man_sniff.wav"]);
+    this._sounds.set(SoundEffect.PlayerResistCold, [ "pls_tone_stick.wav"]);
+    this._sounds.set(SoundEffect.PlayerResistElectric, [ "pls_tone_elec.wav"]);
+    this._sounds.set(SoundEffect.PlayerResistFire, [ "pls_tone_scrape.wav"]);
+    this._sounds.set(SoundEffect.PlayerResistPoison, [ "pls_man_spit.wav"]);
+    this._sounds.set(SoundEffect.PlayerSeeInvisible, [ "pls_tone_clave6.wav"]);
+    this._sounds.set(SoundEffect.PlayerShield, [ "pls_bell_bowl.wav"]);
+    this._sounds.set(SoundEffect.PlayerSlow, [ "pls_man_sigh.wav"]);
+    this._sounds.set(SoundEffect.PlayerStatDrain, [ "pls_tone_headstock.wav"]);
+    this._sounds.set(SoundEffect.Pray, [ "sum_angel_song.wav"]);
+    this._sounds.set(SoundEffect.PseudoId, [ "id_good_hmm.wav"]);
+    this._sounds.set(SoundEffect.Quaff, [ "plm_bottle_clinks.wav", "plm_cork_pop.wav", "plm_cork_squeak.wav"]);
+    this._sounds.set(SoundEffect.QuestMonsterDies, [ "amb_guitar_chord.wav"]);
+    this._sounds.set(SoundEffect.RangedHit, [ "plc_hit_arrow.wav"]);
+    this._sounds.set(SoundEffect.Shoot, [ "plc_miss_swish.wav", "plc_miss_arrow.wav"]);
+    this._sounds.set(SoundEffect.ShutDoor, [ "plm_bang_dumpster.wav", "plm_cabinet_shut.wav", "plm_close_hatch.wav", "plm_door_creakshut.wav", "plm_door_latch.wav", "plm_door_shut.wav", "plm_door_slam.wav"]);
+    this._sounds.set(SoundEffect.StairsDown, [ "plm_floor_creak.wav"]);
+    this._sounds.set(SoundEffect.StairsUp, [ "plm_floor_creak2.wav"]);
+    this._sounds.set(SoundEffect.StoreSoldBargain, [ "id_bad_dang.wav"]);
+    this._sounds.set(SoundEffect.StoreSoldCheaply, [ "sto_man_haha.wav"]);
+    this._sounds.set(SoundEffect.StoreSoldExtraCheaply, [ "sto_man_whoohaha.wav"]);
+    this._sounds.set(SoundEffect.StoreSoldWorthless, [ "sto_man_hey.wav"]);
+    this._sounds.set(SoundEffect.StoreTransaction, [ "sto_coins_countertop.wav", "sto_bell_register1.wav", "sto_bell_register2.wav"]);
+    this._sounds.set(SoundEffect.Study, [ "plm_book_pageturn.wav"]);
+    this._sounds.set(SoundEffect.SummonAngel, [ "sum_angel_song.wav"]);
+    this._sounds.set(SoundEffect.SummonAnimals, [ "sum_lion_growl.wav"]);
+    this._sounds.set(SoundEffect.SummonDemons, [ "sum_ghost_wail.wav", "sum_laugh_evil2.wav"]);
+    this._sounds.set(SoundEffect.SummonDragons, [ "sum_piano_scrape.wav"]);
+    this._sounds.set(SoundEffect.SummonGreaterDemons, [ "sum_ghost_moan.wav"]);
+    this._sounds.set(SoundEffect.SummonGreaterDragons, [ "sum_gong_temple.wav"]);
+    this._sounds.set(SoundEffect.SummonGreaterUndead, [ "sum_ghost_moan.wav"]);
+    this._sounds.set(SoundEffect.SummonHounds, [ "sum_lion_growl.wav"]);
+    this._sounds.set(SoundEffect.SummonHydras, [ "sum_piano_scrape.wav"]);
+    this._sounds.set(SoundEffect.SummonMonster, [ "sum_chime_jangle.wav"]);
+    this._sounds.set(SoundEffect.SummonRingwraiths, [ "sum_bell_hand.wav"]);
+    this._sounds.set(SoundEffect.SummonSpiders, [ "sum_piano_scrape.wav"]);
+    this._sounds.set(SoundEffect.SummonUndead, [ "sum_ghost_oooo.wav"]);
+    this._sounds.set(SoundEffect.SummonUniques, [ "sum_bell_tone.wav"]);
+    this._sounds.set(SoundEffect.Teleport, [ "plm_chimes_jangle.wav"]);
+    this._sounds.set(SoundEffect.TeleportLevel, [ "sum_bell_crystal.wav"]);
+    this._sounds.set(SoundEffect.UniqueDies, [ "sum_ghost_wail.wav"]);
+    this._sounds.set(SoundEffect.UseStaff, [ "plm_use_staff.wav"]);
+    this._sounds.set(SoundEffect.WieldWeapon, [ "plm_metal_sharpen.wav"]);
+    this._sounds.set(SoundEffect.ZapRod, [ "plm_zap_rod.wav"]);
+  }
 
   private check() {
     if (this.connection !== undefined && this.gameGuid !== undefined) {
@@ -162,6 +421,23 @@ export class PlayComponent implements OnInit, OnDestroy {
           });
           this.connection.on("PlaySound", (sound: number) => {
             this._zone.run(() => {
+
+              // Get the list of available sounds.
+              const availableSounds: string[] | undefined = this._sounds.get(sound);
+
+              // Ensure there are sounds available.
+              if (availableSounds !== undefined) {
+                const randomSelection = Math.floor(Math.random() * availableSounds.length);
+
+                // Choose one.
+                var soundResourceName = availableSounds[randomSelection];
+
+                // Play it.
+                const audio = new Audio();
+                audio.src = `/assets/sounds/${soundResourceName}`;
+                audio.load();
+                audio.play();
+              }
             });
           });
           this.connection.on("PlayMusic", (music: number) => {
@@ -184,6 +460,8 @@ export class PlayComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.setupSounds();
+
     // Wait for the authentication.  Games can only be played with authenticated.
     this._initSubscriptions.add(this._authenticationService.currentUser.subscribe((_currentUser: UserDetails | null) => {
       if (_currentUser !== null) {
