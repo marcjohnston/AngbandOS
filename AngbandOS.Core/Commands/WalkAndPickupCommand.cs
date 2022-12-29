03d7d44e245cf0651b68895fe38409d5b23f446a
Marc Johnston <marc-johnston@outlook.com>
2022-12-29 09:16:58 -0500

    Separated the walk commands.

---
+++ AngbandOS.Core/Commands/WalkAndPickupCommand.cs
@@ -0,0 +1,37 @@
+﻿namespace AngbandOS.Commands
+{
+    [Serializable]
+    internal class WalkAndPickupCommand : ICommand
+    {
+        private WalkAndPickupCommand(SaveGame saveGame) { } // This object is a singleton.
+
+        public char Key => ';';
+
+        public int? Repeat => null;
+
+        public bool IsEnabled => true;
+
+        public void Execute(SaveGame saveGame)
+        {
+            DoCmdWalk(saveGame, false);
+        }
+
+        public static void DoCmdWalk(SaveGame saveGame, bool dontPickup)
+        {
+            bool disturb = false;
+            // If we don't already have a direction, get one
+            if (saveGame.GetDirectionNoAim(out int dir))
+            {
+                // Walking takes a full turn
+                saveGame.EnergyUse = 100;
+                saveGame.MovePlayer(dir, dontPickup);
+                disturb = true;
+            }
+            // We will have been disturbed, unless we cancelled the direction prompt
+            if (!disturb)
+            {
+                saveGame.Disturb(false);
+            }
+        }
+    }
+}
---
+++ AngbandOS.Core/Commands/WalkWithoutPickupCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace AngbandOS.Commands
+{
+    [Serializable]
+    internal class WalkWithoutPickupCommand : ICommand
+    {
+        private WalkWithoutPickupCommand(SaveGame saveGame) { } // This object is a singleton.
+
+        public char Key => '-';
+
+        public int? Repeat => null;
+
+        public bool IsEnabled => true;
+
+        public void Execute(SaveGame saveGame)
+        {
+            WalkAndPickupCommand.DoCmdWalk(saveGame, true);
+        }
+    }
+}
