4910df34562e392ee67d98925ea98c6e25b15671
Marc Johnston <marc-johnston@outlook.com>
2023-04-05 23:16:46 -0400

    ApplyRandomSlaying moved to Item.

--- AngbandOS.Core/Items/WeaponItems/BowWeaponItems/BowWeaponItem.cs
+++ AngbandOS.Core/Items/WeaponItems/BowWeaponItems/BowWeaponItem.cs
@@ -3,14 +3,22 @@ namespace AngbandOS.Core.Items
 [Serializable]
     internal abstract class BowWeaponItem : WeaponItem
     {
-        public override void ApplyRandomSlaying(ref IArtifactBias artifactBias, Item item)
+        public override void ApplyRandomSlaying(ref IArtifactBias artifactBias)
         {
+            if (artifactBias != null)
+            {
+                if (artifactBias.ApplySlaying(this))
+                {
+                    return;
+                }
+            }
+
             switch (Program.Rng.DieRoll(6))
             {
                 case 1:
                 case 2:
                 case 3:
-                    item.RandartItemCharacteristics.XtraMight = true;
+                    RandartItemCharacteristics.XtraMight = true;
                     if (artifactBias == null && Program.Rng.DieRoll(9) == 1)
                     {
                         artifactBias = new RangerArtifactBias();
@@ -18,7 +26,7 @@ namespace AngbandOS.Core.Items
                     break;
 
                 default:
-                    item.RandartItemCharacteristics.XtraShots = true;
+                    RandartItemCharacteristics.XtraShots = true;
                     if (artifactBias == null && Program.Rng.DieRoll(9) == 1)
                     {
                         artifactBias = new RangerArtifactBias();
