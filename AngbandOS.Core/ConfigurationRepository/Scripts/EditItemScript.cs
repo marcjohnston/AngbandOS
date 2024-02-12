c1afedfda5a49edcd0245e2710ee3c06d96a3f18
Marc Johnston <marc-johnston@outlook.com>
2024-02-12 11:12:17 -0500

    Added edit item wizard script.

---
+++ AngbandOS.Core/ConfigurationRepository/Scripts/EditItemScript.cs
@@ -0,0 +1,39 @@
+﻿// AngbandOS: 2022 Marc Johnston
+//
+// This game is released under the “Angband License”, defined as: “© 1997 Ben Harrison, James E.
+// Wilson, Robert A. Koeneke This software may be copied and distributed for educational, research,
+// and not for profit purposes provided that this copyright and statement are included in all such
+// copies. Other copyrights may also apply.”
+
+namespace AngbandOS.Core.Scripts;
+
+[Serializable]
+internal class EditItemScript : Script, IScript
+{
+    private EditItemScript(SaveGame saveGame) : base(saveGame) { }
+
+    /// <summary>
+    /// Allows the wizard to edit an item.
+    /// </summary>
+    /// <returns></returns>
+    public void ExecuteScript()
+    {
+        SaveGame.SelectItem(out Item? item, "Select an item to edit:", true, true, true, null);
+        if (item == null)
+        {
+            return;
+        }
+        do
+        {
+            if (!SaveGame.GetString("Rare Item Index: ", out string tmpVal, $"{item.RareItemTypeIndex}", 9))
+            {
+                return;
+            }
+            if (int.TryParse(tmpVal, out int val))
+            {
+                item.RareItemTypeIndex = (RareItemTypeEnum)val;
+                break;
+            }
+        } while (true);
+    }
+}
---
+++ AngbandOS.Core/ConfigurationRepository/WizardCommands/EditItemWizardCommand.cs
@@ -0,0 +1,25 @@
+﻿// AngbandOS: 2022 Marc Johnston
+//
+// This game is released under the “Angband License”, defined as: “© 1997 Ben Harrison, James E.
+// Wilson, Robert A. Koeneke This software may be copied and distributed for educational, research,
+// and not for profit purposes provided that this copyright and statement are included in all such
+// copies. Other copyrights may also apply.”
+
+namespace AngbandOS.Core.WizardCommands;
+
+[Serializable]
+internal class EditItemWizardCommand : WizardCommand
+{
+    private EditItemWizardCommand(SaveGame saveGame) : base(saveGame) { } // This object is a singleton.
+
+    public override char KeyChar => 'I';
+
+    public override string HelpDescription => "Edit Item";
+
+    public override HelpGroup? HelpGroup => SaveGame.SingletonRepository.HelpGroups.Get(nameof(WizardObjectCommandsHelpGroup));
+
+    public override void Execute()
+    {
+        SaveGame.RunScript(nameof(EditItemScript));
+    }
+}
