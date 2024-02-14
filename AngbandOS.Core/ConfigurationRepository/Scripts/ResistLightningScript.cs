adae95f1075c67da3cbc9bb9deb62c195127ef15
Marc Johnston <marc-johnston@outlook.com>
2024-02-14 13:38:24 -0500

    Created ResistLightningScript

--- AngbandOS.Core/ConfigurationRepository/Scripts/ResistLightningScript.cs
+++ AngbandOS.Core/ConfigurationRepository/Scripts/ResistLightningScript.cs
@@ -5,18 +5,19 @@
 // and not for profit purposes provided that this copyright and statement are included in all such
 // copies. Other copyrights may also apply.”
 
-namespace AngbandOS.Core.Spells.Folk;
+namespace AngbandOS.Core.Scripts;
 
 [Serializable]
-internal class FolkSpellResistLightning : Spell
+internal class ResistLightningScript : Script, IScript
 {
-    private FolkSpellResistLightning(SaveGame saveGame) : base(saveGame) { }
-    public override void Cast()
+    private ResistLightningScript(SaveGame saveGame) : base(saveGame) { }
+
+    /// <summary>
+    /// Executes the script.
+    /// </summary>
+    /// <returns></returns>
+    public void ExecuteScript()
     {
-        SaveGame.RunScript(nameof(ResistLightningScript));
+        SaveGame.TimedLightningResistance.AddTimer(SaveGame.DieRoll(20) + 20);
     }
-
-    public override string Name => "Resist Lightning";
-
-    protected override string LearnedDetails => "dur 20+d20";
 }
