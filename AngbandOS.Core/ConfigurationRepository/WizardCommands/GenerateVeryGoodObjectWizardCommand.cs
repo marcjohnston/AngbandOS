46cf6e9f57fe1b1ad617cd7bf58a13a5d2e02c60
Marc Johnston <marc-johnston@outlook.com>
2024-02-18 14:03:23 -0500

    Created CreateVeryGoodObjectScript

--- AngbandOS.Core/ConfigurationRepository/WizardCommands/GenerateVeryGoodObjectWizardCommand.cs
+++ AngbandOS.Core/ConfigurationRepository/WizardCommands/GenerateVeryGoodObjectWizardCommand.cs
@@ -20,10 +20,6 @@ internal class GenerateVeryGoodObjectWizardCommand : WizardCommand
 
     public override void Execute()
     {
-        if (SaveGame.CommandArgument <= 0)
-        {
-            SaveGame.CommandArgument = 1;
-        }
-        SaveGame.Acquirement(SaveGame.MapY, SaveGame.MapX, SaveGame.CommandArgument, true);
+        SaveGame.RunScript(nameof(CreateVeryGoodObjectScript));
     }
 }
