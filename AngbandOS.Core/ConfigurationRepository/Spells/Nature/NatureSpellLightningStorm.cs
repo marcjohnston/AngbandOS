9fe0f9258ebebd63aeab8431802313f1dabb53cb
Marc Johnston <marc-johnston@outlook.com>
2024-02-18 13:56:40 -0500

    Created LightningStormScript

--- AngbandOS.Core/ConfigurationRepository/Spells/Nature/NatureSpellLightningStorm.cs
+++ AngbandOS.Core/ConfigurationRepository/Spells/Nature/NatureSpellLightningStorm.cs
@@ -13,11 +13,7 @@ internal class NatureSpellLightningStorm : Spell
     private NatureSpellLightningStorm(SaveGame saveGame) : base(saveGame) { }
     public override void Cast()
     {
-        if (!SaveGame.GetDirectionWithAim(out int dir))
-        {
-            return;
-        }
-        SaveGame.FireBall(SaveGame.SingletonRepository.Projectiles.Get(nameof(ElecProjectile)), dir, 90 + SaveGame.ExperienceLevel, (SaveGame.ExperienceLevel / 12) + 1);
+        SaveGame.RunScript(nameof(LightningStormScript));
     }
 
     public override string Name => "Lightning Storm";
