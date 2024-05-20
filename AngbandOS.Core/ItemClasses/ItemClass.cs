e3e36a009b0cc49991861b0b0c872efdf27ffb6d
Marc Johnston <marc-johnston@outlook.com>
2024-05-20 16:46:04 -0400

    Rearranged methods.

--- AngbandOS.Core/ItemClasses/ItemClass.cs
+++ AngbandOS.Core/ItemClasses/ItemClass.cs
@@ -10,6 +10,17 @@ internal abstract class ItemClass : IGetKey
     }
 
     /// <summary>
+    /// Returns the capitalized name of a singular item that the class represents.  This name can be used in a format like 'Your {0}'
+    /// </summary>
+    public abstract string Name { get; }
+
+    /// <summary>
+    /// Returns a description of the item class.  This is typically a plural version of the Name property.  This description is typically used to allow the player to select an item from
+    /// the class.
+    /// </summary>
+    public virtual bool AllowStomp => true;
+
+    /// <summary>
     /// Returns the entity serialized into a Json string.
     /// </summary>
     /// <returns></returns>
@@ -18,19 +29,8 @@ internal abstract class ItemClass : IGetKey
         return "";
     }
 
-    /// <summary>
-    /// Returns the capitalized name of a singular item that the class represents.  This name can be used in a format like 'Your {0}'
-    /// </summary>
-    public abstract string Name { get; }
-
     public virtual string Key => GetType().Name;
 
     public string GetKey => Key;
     public virtual void Bind() { }
-
-    /// <summary>
-    /// Returns a description of the item class.  This is typically a plural version of the Name property.  This description is typically used to allow the player to select an item from
-    /// the class.
-    /// </summary>
-    public virtual bool AllowStomp => true;
 }
