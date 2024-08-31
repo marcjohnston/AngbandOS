c596a8ebb6267c7148b55ba6f83c0f133e62f37d
Marc Johnston <marc-johnston@outlook.com>
2024-08-30 22:06:47 -0400

    Ranged weapons EnchantItem done.

--- AngbandOS.Core/ItemFactories/WeaponItemFactories/RangedWeaponItemFactories/LightCrossbowRangedWeaponItemFactory.cs
+++ AngbandOS.Core/ItemFactories/WeaponItemFactories/RangedWeaponItemFactories/LightCrossbowRangedWeaponItemFactory.cs
@@ -32,4 +32,34 @@ internal class LightCrossbowRangedWeaponItemFactory : RangedWeaponItemFactory
         nameof(SteelBoltAmmunitionItemFactory),
         nameof(SeekerBoltAmmunitionItemFactory)
     };
+
+    /// <summary>
+    /// Returns the ranged weapon inventory slot for bows.
+    /// </summary>
+    public override int WieldSlot => InventorySlot.RangedWeapon;
+
+    protected override (int, string)[]? MassProduceTupleNames => new (int, string)[]
+    {
+        (100, "3d5-3")
+    };
+
+    public override bool IsRangedWeapon => true;
+
+    protected override (int[]? Powers, bool? StoreStock, string[] ScriptNames)[]? EnchantmentBinders => new (int[]? Powers, bool? StoreStock, string[] ScriptNames)[]
+   {
+        (new int[] {-2}, null, new string[] { nameof(TerribleHit1D5P5BP10BEnchantmentScript), nameof(TerribleDamage1D5P5BP10BEnchantmentScript), nameof(CursedWeaponEnchantmentScript) }),
+        (new int[] {-1}, null, new string[] { nameof(PoorHit1D5P5BEnchantmentScript), nameof(PoorDamage1D5P5BEnchantmentScript), nameof(CursedWeaponEnchantmentScript) }),
+        (new int[] {1}, null, new string[] { nameof(GoodHit1D5P5BEnchantmentScript), nameof(GoodDamage1D5P5BEnchantmentScript) }),
+        (new int[] {2}, null, new string[] { nameof(GreatHit1D5P5BP10BEnchantmentScript), nameof(GreatDamage1D5P5BP10BEnchantmentScript), nameof(GreatRangedWeaponEnchantmentScript) })
+   };
+
+    public override BaseInventorySlot BaseWieldSlot => Game.SingletonRepository.Get<BaseInventorySlot>(nameof(RangedWeaponInventorySlot));
+    public override bool CanApplyBlowsBonus => true;
+
+    public override int PackSort => 32;
+
+    public override bool CanProjectArrows => true;
+    public override bool HatesFire => true;
+    public override bool HatesAcid => true;
+    public override bool CanApplyArtifactBiasSlaying => false;
 }
--- AngbandOS.Core/Scripts/JewelJudgementDrainLifeScript.cs
+++ AngbandOS.Core/Scripts/JewelJudgementDrainLifeScript.cs
@@ -2905,3 +2905,57 @@ internal class TerribleMeleeWeaponEnchantmentScript : Script, IEnhancementScript
     }
 }
 
+
+[Serializable]
+internal class GreatRangedWeaponEnchantmentScript : Script, IEnhancementScript
+{
+    private GreatRangedWeaponEnchantmentScript(Game game) : base(game) { }
+
+    /// <summary>
+    /// Executes the script.
+    /// </summary>
+    /// <returns></returns>
+    /// <remarks>
+    /// Logic:
+    /// If the chest is on the town level (level == 0 [not sure where the wilderness is]), it is not trapped (default TypeSpecificValue).
+    /// A die roll from 1 to the level of the chest is made.  Any value >55 will convert to a random chest trap between 55 and 63.
+    /// </remarks>
+    public void ExecuteEnchantmentScript(Item item, int level)
+    {
+        switch (Game.DieRoll(21))
+        {
+            case 1:
+            case 11:
+                item.RareItem = Game.SingletonRepository.Get<ItemAdditiveBundle>(nameof(BowOfExtraMightRareItem));
+                item.ApplyRandomResistance(Game.SingletonRepository.Get<ItemAdditiveBundleWeightedRandom>(nameof(ResistanceAndBiasItemAdditiveBundleWeightedRandom)));
+                break;
+            case 2:
+            case 12:
+                item.RareItem = Game.SingletonRepository.Get<ItemAdditiveBundle>(nameof(BowOfExtraShotsRareItem));
+                break;
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+                item.RareItem = Game.SingletonRepository.Get<ItemAdditiveBundle>(nameof(BowOfVelocityRareItem));
+                break;
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+                item.RareItem = Game.SingletonRepository.Get<ItemAdditiveBundle>(nameof(BowOfAccuracyRareItem));
+                break;
+            case 21:
+                item.CreateRandomArtifact(false);
+                break;
+        }
+    }
+}
