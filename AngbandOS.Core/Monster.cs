b2063f4008683f84f06e4b23d29338015bca1543
Marc Johnston <marc-johnston@outlook.com>
2025-03-14 15:59:29 -0400

    Removed unneeded using statement.

--- AngbandOS.Core/Monster.cs
+++ AngbandOS.Core/Monster.cs
@@ -5,8 +5,6 @@
 // and not for profit purposes provided that this copyright and statement are included in all such
 // copies. Other copyrights may also apply.”
 
-using AngbandOS.Core.MonsterRaces;
-
 namespace AngbandOS.Core;
 
 [Serializable]
