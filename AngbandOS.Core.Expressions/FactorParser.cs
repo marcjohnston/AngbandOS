2372142532cfd45ba7fa9bdbb77424afabaec07b
Marc Johnston <marc-johnston@outlook.com>
2025-05-28 13:35:21 -0400

    Added additional comments to the characterIndex parameter.

--- AngbandOS.Core.Expressions/FactorParser.cs
+++ AngbandOS.Core.Expressions/FactorParser.cs
@@ -3,5 +3,12 @@
 [Serializable]
 public abstract class FactorParser
 {
+    /// <summary>
+    /// Attempts to parse a factor from the expression text.  If a factor is not 
+    /// </summary>
+    /// <param name="parser"></param>
+    /// <param name="text"></param>
+    /// <param name="characterIndex">The index at which to start parsing for the factor and returns the new position after the factor has been accepted.  If a factor is not parse, the position should not be changed.</param>
+    /// <returns></returns>
     public abstract Expression? TryParse(ExpressionParser parser, string text, ref int characterIndex);
 }
