128fe2cb0a25f156e89daf9ff81088cdcff6c50a
Marc Johnston <marc-johnston@outlook.com>
2025-06-14 12:34:58 -0400

    Resolved ChaosBolt character class references.

--- AngbandOS.GamePacks.Cthangband/MappedSpellScripts/ChaosBoltSpellChaosRealmSuccessMappedSpellScript.cs
+++
@@ -1,9 +0,0 @@
-namespace AngbandOS.GamePacks.Cthangband;
-
-[Serializable]
-internal class ChaosBoltSpellChaosRealmSuccessMappedSpellScript : MappedSpellScriptGameConfiguration
-{
-    public override string SpellBindingKey => nameof(ChaosBoltChaosSpell);
-    public override string? RealmBindingKey => nameof(ChaosRealm);
-    public override string[]? CastSpellScriptBindingKeys => new string[] { nameof(SystemScriptsEnum.ChaosBoltScript) };
-}
---
+++ AngbandOS.GamePacks.Cthangband/MappedSpellScripts/HighMageCharacterClassChaosBoltSpellChaosRealmSuccessMappedSpellScript.cs
@@ -0,0 +1,10 @@
+﻿namespace AngbandOS.GamePacks.Cthangband;
+
+[Serializable]
+internal class HighMageCharacterClassChaosBoltSpellChaosRealmSuccessMappedSpellScript : MappedSpellScriptGameConfiguration
+{
+    public override string SpellBindingKey => nameof(ChaosBoltChaosSpell);
+    public override string? RealmBindingKey => nameof(ChaosRealm);
+    public override string? CharacterClassBindingKey => nameof(CharacterClassesEnum.HighMageCharacterClass);
+    public override string[]? CastSpellScriptBindingKeys => new string[] { nameof(SystemScriptsEnum.HighMageChaosBoltScript) };
+}
