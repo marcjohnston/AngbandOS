1927d13c0631ab66f6a9a8d627ad8f6a1ab04e69
Marc Johnston <marc-johnston@outlook.com>
2025-06-24 13:48:36 -0400

    Removed duplicate IJson reference.

--- AngbandOS.Core/StoreCommand.cs
+++ AngbandOS.Core/StoreCommand.cs
@@ -7,7 +7,7 @@
 namespace AngbandOS.Core;
 
 [Serializable]
-internal class StoreCommand : IGetKey, IToJson
+internal class StoreCommand : IGetKey
 {
     protected readonly Game Game;
     public StoreCommand(Game game, StoreCommandGameConfiguration storeCommandGameConfiguration)
