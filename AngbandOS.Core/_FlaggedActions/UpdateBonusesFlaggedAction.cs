82b2099a768e1d428a088f831245f72ac85a2d7c
Marc Johnston <marc-johnston@outlook.com>
2025-06-30 10:51:15 -0400

    Reverted the BaseArmorClass removal and unused usings.

--- AngbandOS.Core/_FlaggedActions/UpdateBonusesFlaggedAction.cs
+++ AngbandOS.Core/_FlaggedActions/UpdateBonusesFlaggedAction.cs
@@ -4,9 +4,6 @@
 // Wilson, Robert A. Koeneke This software may be copied and distributed for educational, research,
 // and not for profit purposes provided that this copyright and statement are included in all such
 // copies. Other copyrights may also apply.”
-
-using AngbandOS.Core.RaceAbilities;
-using System.Diagnostics;
 namespace AngbandOS.Core.FlaggedActions;
 
 [Serializable]
