36da316fcbfa07c4f2a973919f45279fc2121074
Marc Johnston <marc-johnston@outlook.com>
2025-08-07 17:16:24 -0400

    Fixed typo

--- AngbandOS.GamePacks.Cthangband/ItemEnhancements/VilyaRingItemFactoryItemEnhancement.cs
+++ AngbandOS.GamePacks.Cthangband/ItemEnhancements/VilyaRingItemFactoryItemEnhancement.cs
@@ -2,6 +2,6 @@ namespace AngbandOS.GamePacks.Cthangband;
     [Serializable]
 public class VilyaRingItemFactoryItemEnhancement : ItemEnhancementGameConfiguration
 {
-ed     public override int Weight => 2;
+     public override int Weight => 2;
     public override int Cost => 300000;
 }
