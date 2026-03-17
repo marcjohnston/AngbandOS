d183a921f3045e6309d68b950c89c394244649c6
Marc Johnston <marc-johnston@outlook.com>
2026-03-17 16:57:03 -0400

    Removed the duplicate method from IConsoleViewPort that are in IViewPort.

--- AngbandOS.Interface/IConsoleViewPort.cs
+++ AngbandOS.Interface/IConsoleViewPort.cs
@@ -11,39 +11,10 @@ public interface IConsoleViewPort : IViewPort
     int MaximumKeyQueueLength { get; }
 
     /// <summary>
-    /// Clear the entire screen.
-    /// </summary>
-    void Clear();
-
-    /// <summary>
-    /// Render output onto the screen.
-    /// </summary>
-    /// <param name="printLines"></param>
-    void BatchPrint(PrintLine[] printLines);
-
-    /// <summary>
     /// Retrieve a keypress from the user.
     /// </summary>
     /// <returns></returns>
     char WaitForKey();
-
-    /// <summary>
-    /// Set the background image.
-    /// </summary>
-    /// <param name="image"></param>
-    void SetBackground(BackgroundImageEnum image);
-
-    /// <summary>
-    /// Play a sound.
-    /// </summary>
-    /// <param name="sound"></param>
-    void PlaySound(SoundEffectEnum sound);
-
-    /// <summary>
-    /// Play music.
-    /// </summary>
-    /// <param name="music"></param>
-    void PlayMusic(MusicTrackEnum music);
 
     bool KeyQueueIsEmpty();
 
