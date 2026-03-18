bbba1bb987f6fdd9ce261bdd6885544c84f07b7d
Marc Johnston <marc-johnston@outlook.com>
2026-03-18 15:09:33 -0400

    Updated version to 1.6

--- AngbandOS.Web/ClientApp/src/environments/environment.prod.ts
+++ AngbandOS.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  version: "v1.5"
+  version: "v1.6"
 };
--- AngbandOS.Web/ClientApp/src/environments/environment.ts
+++ AngbandOS.Web/ClientApp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  version: "v1.5"
+  version: "v1.6"
 };
 
 /*
