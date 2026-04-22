67e69650f0ff9aa607ad6fa15a3c6580ac584edc
Marc Johnston <marc-johnston@outlook.com>
2026-04-21 22:37:35 -0400

    Fixed issue with no genders available for random.

--- AngbandOS.Core.Interface.Configuration/GenderGameConfiguration.cs
+++ AngbandOS.Core.Interface.Configuration/GenderGameConfiguration.cs
@@ -6,5 +6,5 @@ public class GenderGameConfiguration : NonCompositeSingletonGameConfiguration
     public virtual string Title { get; set; }
     public virtual string Winner { get; set; } // TODO ... this winner title to describe the type of winner is not rendered
 
-    public virtual bool CanBeRandomlySelected { get; set; }
+    public virtual bool CanBeRandomlySelected { get; set; } = true;
 }
