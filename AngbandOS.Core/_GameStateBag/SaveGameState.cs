403d950f15563176224e2b44966d8b0e65e13a57
Marc Johnston <marc-johnston@outlook.com>
2026-05-21 17:26:13 -0400

    GameSerializable[][] is now nullable.

--- AngbandOS.Core/_GameStateBag/SaveGameState.cs
+++ AngbandOS.Core/_GameStateBag/SaveGameState.cs
@@ -157,8 +157,13 @@ internal class SaveGameState
         return new ListGameStateBag(gameStateBags.ToArray());
     }
 
-    public GameStateBag CreateGameStateBag(IGameSerialize[][] gameSerializable, Type derivedType, params Type[] derivedTypes)
+    public GameStateBag CreateGameStateBag(IGameSerialize[][]? gameSerializable, Type derivedType, params Type[] derivedTypes)
     {
+        if (gameSerializable is null)
+        {
+            return new NullValueGameStateBag();
+        }
+
         var gameStateBags = new List<GameStateBag>();
         foreach (IGameSerialize[] item in gameSerializable)
         {
