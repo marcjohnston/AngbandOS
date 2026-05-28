3643bc372c0d0ccf896b8b9d31c56c608eb338fd
Marc Johnston <marc-johnston@outlook.com>
2026-05-28 16:02:00 -0400

    Added a handful of XML comments.

--- AngbandOS.Core.Expressions/Expression.cs
+++ AngbandOS.Core.Expressions/Expression.cs
@@ -4,11 +4,14 @@
 public abstract class Expression
 {
     /// <summary>
-    /// Computes and returns the result.
+    /// Represents the method that all derived expression need to implement to compute the result of the expression.
     /// </summary>
     /// <returns></returns>
     public abstract Expression Compute();
 
+    /// <summary>
+    /// Represents the types of results that the expression can produce that must be implemented by all derived expressions.
+    /// </summary>
     public abstract Type[] ResultTypes { get; }
 
     /// <summary>
@@ -36,7 +39,22 @@ public abstract class Expression
         }
         throw new Exception($"Invalid result from Expression.Compute on {expression.ToString()}.  A result of type {typeof(TExpression).Name} was expected.");
     }
+
+    /// <summary>
+    /// Represents the method that all derived expression must implement to return the text representation of the expression.
+    /// </summary>
     public abstract string Text { get; }
+
+    /// <summary>
+    /// Returns the Text representation of the expression for debugging purposes.
+    /// </summary>
+    /// <returns></returns>
     public override string ToString() => Text;
+
+    /// <summary>
+    /// Represents the method that all derived expression can override to minimize the expression.  The default implementation is to return the expression itself without any changes.
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
     public virtual Expression Minimize(MinimizeOptions? options = null) => this; // TODO: Would like the options to not be required to be sent.
 }
--- AngbandOS.Core/_Expressions/DiceRollInfixExpression.cs
+++ AngbandOS.Core/_Expressions/DiceRollInfixExpression.cs
@@ -3,14 +3,34 @@
 [Serializable]
 internal class DiceRollInfixExpression : InfixExpression
 {
+    /// <summary>
+    /// Returns the game object.  This is needed for access to the Random to compute the dice roll.
+    /// </summary>
     public Game Game { get; }
     public DiceRollInfixExpression(Game game, Expression dice, Expression sides) : base(dice, sides)
     {
         Game = game;
     }
+
+    /// <summary>
+    /// The number of dice to roll.  This is the left operand of the infix operator.  For example, in "3d6", the dice is 3.  In "d6", the dice is 1.
+    /// </summary>
     public Expression Dice => base.Operand1;
+
+    /// <summary>
+    /// The number of sides on each die.  This is the right operand of the infix operator.  For example, in "3d6", the sides is 6.
+    /// </summary>
     public Expression Sides => base.Operand2;
+
+    /// <summary>
+    /// Returns the result types of the expression.  In this case, the result type is an integer expression since a dice roll will always return an integer value.
+    /// </summary>
     public override Type[] ResultTypes => new Type[] { typeof(IntegerExpression) };
+
+    /// <summary>
+    /// Compute the result of the dice roll.  This will compute the number of dice to roll and the number of sides on each die, then use the game's random number generator to simulate rolling the dice and summing the results.  The final result is returned as an IntegerExpression.
+    /// </summary>
+    /// <returns></returns>
     public override Expression Compute()
     {
         IntegerExpression diceIntegerExpression = Game.ComputeIntegerExpression(Dice);
@@ -24,6 +44,10 @@ internal class DiceRollInfixExpression : InfixExpression
         }
         return new IntegerExpression(sum);
     }
+
+    /// <summary>
+    /// Returns the text representation of the dice roll expression.  If the number of dice is 1, then the "1" is omitted for brevity.  For example, "d6" is used instead of "1d6".  If the number of dice is not 1, then the full representation is used.  For example, "3d6" is used instead of "d6".
+    /// </summary>
     public override string Text
     {
         get
@@ -39,6 +63,12 @@ internal class DiceRollInfixExpression : InfixExpression
             return $"{Dice}d{Sides}";
         }
     }
+
+    /// <summary>
+    /// Returns a minimized version of the dice roll expression.  The minimization process performs a minimization on the dice and the sides, and returns a new diceDsides expression.
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
     public override Expression Minimize(MinimizeOptions? options = null)
     {
         Expression minimizedDice = Dice.Minimize(options);
