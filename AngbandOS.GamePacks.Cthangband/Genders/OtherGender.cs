07fd96ca1f464284c4c09b2b87cbfc9368b64089
Marc Johnston <marc-johnston@outlook.com>
2026-06-04 16:23:42 -0400

    Removed blank line.

--- AngbandOS.GamePacks.Cthangband/Genders/OtherGender.cs
+++ AngbandOS.GamePacks.Cthangband/Genders/OtherGender.cs
@@ -16,5 +16,4 @@ public class OtherGender : GenderGameConfiguration
     /// Returns false, because the Other gender shouldn't be selected when a random character is chosen.
     /// </summary>
     public override bool CanBeRandomlySelected => false;
-
 }
