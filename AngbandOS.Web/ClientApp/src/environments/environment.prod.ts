ec422cac4dc64e18f3235425977d18e0f227e61a
Marc Johnston <marc-johnston@outlook.com>
2026-07-14 22:44:32 -0400

    Updated to v1.9

--- AngbandOS.Web/ClientApp/src/environments/environment.prod.ts
+++ AngbandOS.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  version: "v1.8"
+  version: "v1.9"
 };
--- AngbandOS.Web/ClientApp/src/environments/environment.ts
+++ AngbandOS.Web/ClientApp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  version: "v1.8"
+  version: "v1.9"
 };
 
 /*
