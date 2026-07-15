cebe9e0eb6bd41687b54cf68896572ad81e0d5a9
Marc Johnston <marc-johnston@outlook.com>
2026-07-15 09:25:54 -0400

    Removed the Game.GetStackTokens method.

--- AngbandOS.Core/Game.cs
+++ AngbandOS.Core/Game.cs
@@ -4,7 +4,6 @@
 // Wilson, Robert A. Koeneke This software may be copied and distributed for educational, research,
 // and not for profit purposes provided that this copyright and statement are included in all such
 // copies. Other copyrights may also apply.”
-using System.Diagnostics;
 using System.Reflection;
 namespace AngbandOS.Core;
 
@@ -9095,42 +9094,6 @@ internal partial class Game : IGameSerialize
         {
             ReplayPersistentStorage.WriteStep(DateTime.Now.ToUniversalTime(), keystroke, _mainSequence.CurrentSeed, null);
         }
-    }
-
-    public static string[] GetStackTokens(bool onlyMyCode = true)
-    {
-        var trace = new StackTrace(true);
-        var frames = trace.GetFrames();
-
-        if (frames == null)
-        {
-            return Array.Empty<string>();
-        }
-
-        var tokens = new List<string>();
-        var myAsm = Assembly.GetExecutingAssembly();
-
-        foreach (var frame in frames)
-        {
-            var method = frame.GetMethod();
-            var type = method?.DeclaringType;
-
-            if (method == null || type == null)
-            {
-                continue;
-            }
-
-            if (onlyMyCode && type.Assembly != myAsm)
-            {
-                continue;
-            }
-
-            var line = frame.GetFileLineNumber(); // <-- key part
-
-            tokens.Add($"{type.FullName}.{method.Name}:{line}");
-        }
-
-        return tokens.ToArray();
     }
 
     public char GetAndRecordKeystroke(bool disableArtificialKeyBuffer = false, bool nonBlocking = false)
