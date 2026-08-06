da1ce6567f1a4809f050f6244a5e7b280c76e0ca
Marc Johnston <marc-johnston@outlook.com>
2026-08-06 15:02:55 -0400

    Updated console table.

--- AngbandOS.Core/_ConsoleElements/ConsoleTableWithRowHighlighting.cs
+++ AngbandOS.Core/_ConsoleElements/ConsoleTableWithRowHighlighting.cs
@@ -6,11 +6,16 @@
 // copies. Other copyrights may also apply.”
 namespace AngbandOS.Core.ConsoleElements;
 
+/// <summary>
+/// The first row is zero-based.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 internal class ConsoleTableWithRowHighlighting<T> : ConsoleTable
 {
     private ColorEnum HighlightRowColor { get; }
     private ColorEnum DefaultRowColor { get; }
     private int? CurrentHighLitRow = null;
+    private T[] RowSource { get; }
     private void HighlightRow(int rowIndex, ColorEnum color)
     {
         foreach (ConsoleTableColumn column in Columns)
@@ -19,6 +24,8 @@ internal class ConsoleTableWithRowHighlighting<T> : ConsoleTable
             consoleString.SetColor(color);
         }
     }
+
+    public T? CurrentRow => CurrentHighLitRow is null ? default : RowSource[CurrentHighLitRow.Value];
 
     /// <summary>
     /// Highlight a row or remove the row highlighting.  Specify null, to remove the row highlighting.
@@ -43,6 +50,7 @@ internal class ConsoleTableWithRowHighlighting<T> : ConsoleTable
     {
         DefaultRowColor = defaultRowColor;
         HighlightRowColor = highlightRowColor;
+        RowSource = rowSource;
 
         foreach ((string Name, Func<T, string> GetRowValue) in columns)
         {
