d6d03290a4382c5fab0145c6f75bf04312b83363
Marc Johnston <marc-johnston@outlook.com>
2026-08-20 17:17:35 -0400

    Removed unused ItemEnhancement.

--- AngbandOS.GamePacks.Cthangband/ItemEnhancements/ItemEnhancement.cs
+++
@@ -1,9 +0,0 @@
-namespace AngbandOS.GamePacks.Cthangband;
-
-public class ItemEnhancement : ItemEnhancementGameConfiguration
-{
-    public override (string AttributeName, string Expression)[]? SummationAttributeAndExpressionBindings => new (string AttributeName, string Expression)[]
-    {
-        (nameof(WeightAttribute), "50"),
-    };
-}
