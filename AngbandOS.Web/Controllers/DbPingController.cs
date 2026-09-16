af41ebc8c081f0f112c6b82f7621ad591a51a53b
Kinjal Tank <kinjal.tank4@outlook.com>
2026-09-16 15:29:59 -0500

    Created IMCP and DB ping controllers to monitor website status

---
+++ AngbandOS.Web/Controllers/DbPingController.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Security.Claims;
+using System.Text.Json;
+using AngbandOS.Core.Interface;
+using AngbandOS.Core.Interface.Configuration;
+using AngbandOS.Core.Interface.Metadata;
+using AngbandOS.PersistentStorage;
+using AngbandOS.Web.Data;
+using AngbandOS.Web.Interface;
+using AngbandOS.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngbandOS.Web.Controllers
+{
+    [Route("apiv1")]
+    [ApiController]
+    [Produces("application/json")]
+
+    public class DbPingController : ControllerBase
+    {
+        private readonly ApplicationDbContext DbContext;
+
+        public DbPingController(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("db-ping")]
+        [Produces("application/json")]
+        [AllowAnonymous]
+
+        public async Task<ActionResult<string>> DbPing()
+        {
+            try
+            {
+                bool canConnect = await DbContext.Database.CanConnectAsync();
+                if (canConnect)
+                {
+                    return StatusCode(200, "pk");
+                }
+                else
+                {
+                    return StatusCode(503, "unavailable");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(503, "unavailable");
+            }
+        }
+    }
+}
---
+++ AngbandOS.Web/Controllers/IMCPController.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+using AngbandOS.Core.Interface;
+using AngbandOS.Core.Interface.Configuration;
+using AngbandOS.Core.Interface.Metadata;
+using AngbandOS.PersistentStorage;
+using AngbandOS.Web.Data;
+using AngbandOS.Web.Interface;
+using AngbandOS.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngbandOS.Web.Controllers
+{
+    [Route("apiv1")]
+    [ApiController]
+    [Produces("application/json")]
+    public class IMCPController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ping")]
+        [Produces("application/json")]
+        [AllowAnonymous]
+        public ActionResult<string> Ping()
+        {
+            return Ok("OK");
+        }
+    }
+}
